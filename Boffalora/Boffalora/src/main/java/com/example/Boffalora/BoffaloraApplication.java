65345693df152b79d6f81f5119159022b985aac5
MarcoGuerra <marco.guerra@finconsgroup.com>
2025-02-16 15:22:45 +0100

    primo commit

---
+++ Boffalora/Boffalora/src/main/java/com/example/Boffalora/BoffaloraApplication.java
@@ -0,0 +1,13 @@
+package com.example.Boffalora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoffaloraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoffaloraApplication.class, args);
+	}
+
+}
---
+++ Boffalora/Boffalora/src/test/java/com/example/Boffalora/BoffaloraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Boffalora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoffaloraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
