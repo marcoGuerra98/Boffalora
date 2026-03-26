ab6f762f970b8a002497c61c632d64f4ed5c3dfd
marcoGuerra98 <marcoguerra098@gmail.com>
2026-03-26 16:56:59 +0100

    Add files via upload

---
+++ project/project/src/test/java/com/boffa/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boffa.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
